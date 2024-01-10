c4010b0428af08406609272db4aa653836cf7099
Maria Adner <juergens.maria@googlemail.com>
2024-01-10 10:55:48 +0100

    Form angelegt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  return <Form />;
+}
+
+function Form() {
+  const [activities, setActivies] = useState("");
+  const [goodWeather, setGoodWeather] = useState(false);
+
+  function handleAddActivity(event) {
+    event.preventDefault();
+    const newActivity = {id:...} 
+
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Plan your Activities</h1>
+      <h3>Add new Activity: </h3>
+      <form onSubmit={handleAddActivity}>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={activities}
+          onChange={(event) => setActivies(event.target.value)}
+        ></input>
+        <br></br>
+        <label>Good weather activity:</label>
+        <input
+          type="checkbox"
+          checked={goodWeather}
+          onChange={(event) => setGoodWeather(event.target.checked)}
+        ></input>
+        <br></br>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
-
-export default App;
